d8aaa938cf0b145ac7eb6e39b7fa139d8930c84e
illulia <choidaehoon0507@gmail.com>
2020-04-06 17:48:42 +0900

    .

---
+++ 5543.py
@@ -0,0 +1,14 @@
+b = 2000
+d = 2000
+for i in range(3):
+    a = int(input())
+    if a < b:
+        b = a
+for i in range(2):
+    c = int(input())
+    if c < d:
+        d = c
+print(b+d-50)
+
+
+    
