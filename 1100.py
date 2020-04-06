4563e0f67e00a415a0bafd088db7cc84331d8455
illulia <choidaehoon0507@gmail.com>
2020-04-06 19:46:21 +0900

    .

---
+++ 1100.py
@@ -0,0 +1,11 @@
+White_list = []
+x = "F"
+for i in range(4):
+    a = input()
+    for j in (0,2,4,6):
+        White_list.append(a[j])
+    b = input()
+    for k in (1,3,5,7):
+        White_list.append(b[k])
+print(White_list.count(x))
+
