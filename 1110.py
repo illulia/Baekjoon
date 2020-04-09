93edc811ec0608a89688c527441d5d3f470bd4f9
illulia <choidaehoon0507@gmail.com>
2020-04-09 15:38:18 +0900

    .

--- 1110.py
+++ 1110.py
@@ -1,3 +1,13 @@
-import sys
-a = int(sys.stdin.readline())
-b = a%10
+a = int(input())
+cheak = a
+b = 0
+temp = 0
+aount = 0
+while True:
+    temp = a//10 + a%10
+    b = (a%10)*10 + temp%10
+    aount += 1
+    a = b
+    if b == cheak:
+        break
+print(aount)
