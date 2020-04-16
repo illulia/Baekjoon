6ecdacacf703fb62a9e83af029c51bdf0d7eecc2
illulia <choidaehoon0507@gmail.com>
2020-04-16 20:05:15 +0900

    .

---
+++ 3052.py
@@ -0,0 +1,7 @@
+a = list()
+for i in range(10):
+    a.append(input())
+a = set(a)
+print(a)
+    
+
--- 4344.py
+++ 4344.py
@@ -1,10 +1,10 @@
-for i in range(int(input())):
+a = int(input())
+for i in range(a):
     num = 0
     b = list(map(int,input().split()))
     c = sum(b[1:]) / b[0]
-    for j in range(len(b)):
+    for j in range(1,len(b)):
         if b[j] > c:
             num += 1
     print(str('%.3f' % round(num/b[0]*100, 3))+'%')
             
-
